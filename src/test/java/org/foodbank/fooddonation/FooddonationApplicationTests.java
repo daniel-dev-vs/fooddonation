eefbda7c8f78925154c6aa66c1ec84aaa2f027d6
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2025-06-26 20:06:01 -0300

    first commit

---
+++ src/test/java/org/foodbank/fooddonation/FooddonationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foodbank.fooddonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
